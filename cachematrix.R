de6628efa5a17c4f791cbd0e99b44025e4dc310e
Hema Prasad <hemselvan@yahoo.com>
2017-07-20 23:16:56 +1000

    Assignment2 - Lexicon Scoping Code
    Lexicon Scoping Code

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
 makeCacheMatrix <- function( x = matrix() ) {
+  ## x is a matrix that is invertible
+  ## set function is to set the matrix
+  ## get function is to get the matrix
+  ## setInverse function is to set the inverse of matrix
+  ## getInverse function is to get the inverse of the matrix
   
   inv <- NULL ## Initialize inverse to null
   set <- function(y) {
     x <<- y
-    i <<- NULL
+    inv <<- NULL
   }
   
-  get = function() x ## This function gets the matrix
+  get <- function() x ## This function gets the matrix
   
-  setInverse <- function(inverse)   i <<- inverse ## This function is to set the inverse of the matrix
- 
-  getInverse <- function() i ## This function is to "get" the inverse matrix
-
+  setInverse <- function(inverse) inv <<- inverse ## This function is to set the inverse of the matrix
+  
+  getInverse <- function() inv ## This function is to "get" the inverse matrix
+  
   list(set = set, get = get,
        setInverse = setInverse,
        getInverse = getInverse)
@@ -23,7 +26,7 @@ cacheSolve <- function(x, ...) {
   ## x is the output of makeCacheMatrix function
   ## cachesolve returns the inverse of the matrix which is input to makeCacheMatrix function
   
-  inv = x$getinv()
+  inv <- x$getInverse()
   
   # if the inverse has already been calculated
   if (!is.null(inv)){
@@ -33,11 +36,11 @@ cacheSolve <- function(x, ...) {
   }
   
   # otherwise, it means that the inverse is not in the cache, hence compute the inverse 
-  mat.data = x$get()
-  inv = solve(mat.data,...)
+  mat.data <- x$get()
+  inv <- solve(mat.data,...)
   
   # sets the value of the inverse in the cache via the setinv function.
-  x$setinv(inv)
+  x$setInverse(inv)
   
-  return(inv)
+  inv
 }
